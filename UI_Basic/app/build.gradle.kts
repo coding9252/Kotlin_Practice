99059479bf85dfa33afdf729e40a438bb3627190
Gahyeon <coding9252@naver.com>
2023-12-16 20:43:11 +0900

    init

--- UI_Basic/app/build.gradle.kts
+++ UI_Basic/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.android.ui_basic"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.ui_basic"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
