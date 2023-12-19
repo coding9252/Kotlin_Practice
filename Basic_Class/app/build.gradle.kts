146522406ca1ef6555c6c4237da0a3155e8cd82c
Gahyeon <coding9252@naver.com>
2023-12-19 20:46:00 +0900

    test

--- Basic_Class/app/build.gradle.kts
+++ Basic_Class/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.android.basic_class"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.basic_class"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
